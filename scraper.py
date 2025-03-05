4bfa309039fea9f84cd1ed455035f233b82ba7e4
bobm-stack <bobleblob12345@gmail.com>
2025-03-05 12:00:16 +0100

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -9,7 +9,8 @@ def urls(url, keyword):
   try:
     res = requests.get(url)
   except:
-    pass
+    print(f"the request failed at {url}")
+    return
 if response.status_code == 200 :
   soup = BeautifulSoup(res.content,"html.parser")
   atag = soup.find_all("a")
@@ -18,9 +19,18 @@ if response.status_code == 200 :
     href = tag.get("href")
     if href is not None and href != "":
       url_found.append(href)
-  print(url_found)
-    
 
+
+for urls2 in url_found:  
+  if urls2 not in already_seen():
+    already_seen.add(url)
+    url_join  = urljoin(url,urls2)
+    if keyword in url_join:
+      print(url_join)
+      urls(url_join,keyword)
+    else:
+      pass
+    
 
 url = input(" enter the url u want to search for: ")
 keyword = ("teh keyword u want to find: ")
